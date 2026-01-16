c97c5dd69cf68d291324cc4a14dbc16fb864c013
Thanos Tasiopoulos <thanos.tasiopoulos@gmail.com>
2026-01-16 20:18:23 +0200

    corrections

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-new Typed("#typed", {
-  strings: [
-    "Senior Data Analyst",
-    "Business Intelligence Specialist",
-    "SQL & Data Modeling Expert",
-    "From Raw Data to Strategic Insights"
-  ],
-  typeSpeed: 60,
-  backSpeed: 40,
-  loop: true
+document.addEventListener('DOMContentLoaded', function() {
+  new Typed("#typed", {
+    strings: [
+      "MSc in Information Systems",
+      "From Raw Data to Strategic Insights", 
+    ],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop: true,
+    
+    
+  });
 });
