a885613303ad2d42658f71d3d3ff70c55adfc1e2
Athanasios Tasiopoulos <thanos.tasiopoulos@gmail.com>
2026-01-16 16:18:58 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+new Typed("#typed", {
+  strings: [
+    "Senior Data Analyst",
+    "Business Intelligence Specialist",
+    "SQL & Data Modeling Expert",
+    "From Raw Data to Strategic Insights"
+  ],
+  typeSpeed: 60,
+  backSpeed: 40,
+  loop: true
+});
